507e313c6575bc0663959ba6a734464c72bfcb86
ariefakbarh <ariefakbarh>
2019-03-07 19:53:02 +0700

    final Project

---
+++ javascript/page.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+        $('li').click(function (e) {
+            $('li').removeClass('active');
+            $(this).addClass('active');                
+        });            
+    });
